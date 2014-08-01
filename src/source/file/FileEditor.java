5efcfe0056ce3239a7a109a4665bf8969beb6250
Tyler Weaver <tweaver@lgsinnovations.com>
2014-08-01 11:27:46 -0600

    initial commit

---
+++ src/source/file/FileEditor.java
@@ -0,0 +1,41 @@
+package source.file;
+
+import gui.control.JFrequencyControl;
+import source.SourceEditor;
+import source.config.SourceConfigFile;
+import source.config.SourceConfiguration;
+
+public class FileEditor extends SourceEditor 
+{
+    private static final long serialVersionUID = 1L;
+    private JFrequencyControl mFrequencyControl;
+    
+	public FileEditor( SourceConfiguration config )
+	{
+		super( config );
+		
+		initGUI();
+	}
+
+	public void reset()
+	{
+		mFrequencyControl.setFrequency( 
+				((SourceConfigFile)mConfig).getFrequency(), false );
+	}
+	
+	public void save()
+	{
+		((SourceConfigFile)mConfig).setFrequency( mFrequencyControl.getFrequency() );
+	}
+	
+	private void initGUI()
+	{
+		
+		mFrequencyControl = new JFrequencyControl();
+		
+		mFrequencyControl.setFrequency( 
+				((SourceConfigFile)mConfig).getFrequency(), false );
+		
+		add( mFrequencyControl );
+	}
+}
---
+++ src/source/file/FileNode.java
@@ -0,0 +1,18 @@
+package source.file;
+
+import controller.BaseNode;
+
+public class FileNode extends BaseNode
+{
+    private static final long serialVersionUID = 1L;
+
+    public FileNode()
+	{
+        super( null );
+	}
+    
+    public String toString()
+    {
+    	return "File Node";
+    }
+}
